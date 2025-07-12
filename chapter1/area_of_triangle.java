4c0641e3266c37cbf9650f3c677d41ad5596729e
Adityamangate55 <adityamangate55@gmail.com>
2025-07-12 11:32:19 +0530

    Add files via upload

---
+++ chapter1/area_of_triangle.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class area_of_triangle{
+    public static void main(String args[]) {
+        int b,h;
+        double a;
+        Scanner s=new Scanner(System.in);
+        System.out.println("Enter the breadth");
+        b=s.nextInt();
+        System.out.println("Enter the height");
+        h=s.nextInt();
+        a=0.5*b*h;
+        System.out.println("The area of triangle is "+a);
+    }
+}
