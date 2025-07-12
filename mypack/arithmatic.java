7b9ca4f9a2e52da4721dddc43651c02b614a8267
Adityamangate55 <adityamangate55@gmail.com>
2025-07-12 11:56:45 +0530

    Add files via upload

---
+++ mypack/arithmatic.java
@@ -0,0 +1,13 @@
+package mypack;
+public class arithmatic{
+    public void add(int a, int b){
+        int c;
+        c=a+b;
+        System.out.println("the addtion is "+c);
+    }
+    public void sub(int a, int b){
+        int c;
+        c=a-b;
+        System.out.println("the substraction is "+c);
+    }
+}
